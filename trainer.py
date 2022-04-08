23bbfd234b94e5037bf7de20a340824750113213
gianlucagiudice <gianlu.giudice@gmail.com>
2022-04-08 13:32:23 +0200

    Fix device usage

--- trainer.py
+++ trainer.py
@@ -11,11 +11,9 @@ import wandb
 
 def train(args):
     seed_list = copy.deepcopy(args['seed'])
-    device = copy.deepcopy(args['device'])
 
     for seed in seed_list:
         args['seed'] = seed
-        args['device'] = device
         _train(args)
 
 
