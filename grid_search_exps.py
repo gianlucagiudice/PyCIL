3a6b643a9016a29faf66994240cbfc9bf0456e17
gianlucagiudice <gianlu.giudice@gmail.com>
2022-05-10 22:51:52 +0200

    Add CIL task parameters

--- grid_search_exps.py
+++ grid_search_exps.py
@@ -12,30 +12,43 @@ parser.add_argument('--baseline', action=argparse.BooleanOptionalAction,
 parser.add_argument('--grid-ids', nargs="+", type=int, default=None,
                     help='Gridsearch ids.', required=False)
 
-
 parser.add_argument('--interactive', action=argparse.BooleanOptionalAction,
                     help='Gridsearch for baseline.', required=False, default=False)
+
+parser.add_argument('--init-cls', type=int, required=True, default=30,
+                    help='Number of init classes.')
+
+parser.add_argument('--increment-cls', type=int, required=True, default=30,
+                    help='Number of classes for each incremental task.')
+
+parser.add_argument('--memory-per-class', type=int, required=False, default=1000,
+                    help='Number of memory per classes for each incremental task.')
 
 args = parser.parse_args()
 
 config_dict = {
-    "run_name": "exp02_docker-top-frequency-nopretrained-50",
+    "run_name": None,
     "prefix": "reproduce",
-    "dataset": "LogoDet-3K_cropped",
-    "memory_size": 2000,
-    "memory_per_class": 1000,
-    "fixed_memory": True,
-    "shuffle": True,
-    "init_cls": 100 if args.baseline else 30,
-    "increment": 10,
     "model_name": "der",
-    "data_augmentation": True,
+    "dataset": "LogoDet-3K_cropped",
+    "shuffle": True,
     "seed": [830694],
+
+    "data_augmentation": True,
+    "convnet_type": "resnet34",
+    "pretrained": True,
+
+    # CIL task
+    "init_cls": args.init_cls,
+    "increment": args.increment_cls,
+
+    # Memory for CIL tasks
+    "memory_size": 2000,
+    "memory_per_class": args.memory_per_class,
+    "fixed_memory": True,
 
     # Grid search parameters
     "dropout": 0,
-    "convnet_type": None,
-    "pretrained": None,
 
     # Adam optimizer
     "adam": True,
@@ -53,12 +66,10 @@ config_dict = {
 grid_search_path = "exps/CIL_LogoDet-3k_grid_search.json"
 
 grid_search = [
-    # Architecture
-    ["resnet34"],
-    # Pretrained
-    [True, False],
     # Dropout rate
-    [0.5, 0.3, 0.1]
+    [0.5, 0.3, 0.1],
+    # Pretrained
+    [True]
 ]
 grid_search = list(itertools.product(*grid_search))
 
@@ -83,7 +94,9 @@ for (i, element) in enumerate(grid_search, 1):
     print(f'{"=" * 20} Grid search {i}/{len(grid_search)} {"=" * 20}')
 
     # Unpack gridsearch
-    architecture, pretrained, dropout = element
+    dropout, _ = element
+    architecture = config_dict['convnet_type']
+    pretrained = config_dict['pretrained']
 
     # Construct dict parameters
     config_dict_temp = copy(config_dict)
